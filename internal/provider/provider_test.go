d970251e7271db9344b80af0ec7fc8ac34309b39
Todd Knight <T.Alan.Knight@gmail.com>
2020-05-18 09:32:48 -0700

    Add provider internal validate test.

---
+++ internal/provider/provider_test.go
@@ -0,0 +1,22 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+var testProviders = map[string]terraform.ResourceProvider{
+	"watchtower": New(),
+}
+
+func testProvider() terraform.ResourceProvider {
+	return testProviders["watchtower"]
+}
+
+func TestProvider(t *testing.T) {
+	if err := New().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
