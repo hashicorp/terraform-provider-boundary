86950be7b0182c93993e3780e4bb7504d7ff4167
Todd Knight <T.Alan.Knight@gmail.com>
2020-05-18 09:29:20 -0700

    Add watchtower provider using the auto generated go sdk.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hashicorp/terraform-provider-scaffolding
 
 go 1.12
 
-require github.com/hashicorp/terraform-plugin-sdk v1.4.1
+require (
+	github.com/hashicorp/terraform-plugin-sdk v1.4.1
+	github.com/hashicorp/watchtower v0.0.0-20200516235313-fad302a36d67
+)
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,17 +1,54 @@
 package provider
 
 import (
+	"context"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/watchtower/api"
 )
 
 func New() terraform.ResourceProvider {
-	return &schema.Provider{
-		DataSourcesMap: map[string]*schema.Resource{
-			"scaffolding_data_source": dataSourceScaffolding(),
+	p := &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"organization": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("WATCHTOWER_ORGANIZATION", nil),
+				Description: "The Watchtower organization scope to operate all actions in.",
+			},
+			"base_url": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The base url of the Watchtower API.  For example 'http://127.0.0.1/'",
+			},
 		},
-		ResourcesMap: map[string]*schema.Resource{
-			"scaffolding_resource": resourceScaffolding(),
-		},
+	}
+
+	p.ConfigureFunc = providerConfigure(p)
+
+	return p
+}
+
+type metaData struct {
+	client *api.Client
+	ctx    context.Context
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
+	return func(d *schema.ResourceData) (interface{}, error) {
+		client, err := api.NewClient(nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := client.SetAddr(d.Get("base_url").(string)); err != nil {
+			return nil, err
+		}
+		client.SetOrg(d.Get("organization").(string))
+
+		// TODO: Pass these in through the config, add token, etc...
+		client.SetLimiter(5, 5)
+
+		return &metaData{client: client, ctx: p.StopContext()}, nil
 	}
 }
