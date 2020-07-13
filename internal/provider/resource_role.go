43d23f179e02bf015483141d688c7d33bff05e58
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-07-12 23:28:07 -0400

    Update provider and tests for combined user/groups into principals in role (#15)

--- internal/provider/resource_role.go
+++ internal/provider/resource_role.go
@@ -11,8 +11,7 @@ import (
 const (
 	roleNameKey        = "name"
 	roleDescriptionKey = "description"
-	roleUsersKey       = "users"
-	roleGroupsKey      = "groups"
+	rolePrincipalsKey  = "principals"
 	roleGrantsKey      = "grants"
 )
 
@@ -31,12 +30,7 @@ func resourceRole() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			roleUsersKey: {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem:     &schema.Schema{Type: schema.TypeString},
-			},
-			roleGroupsKey: {
+			rolePrincipalsKey: {
 				Type:     schema.TypeSet,
 				Optional: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
@@ -48,7 +42,6 @@ func resourceRole() *schema.Resource {
 			},
 		},
 	}
-
 }
 
 // convertRoleToResourceData creates a ResourceData type from a Role
@@ -65,14 +58,8 @@ func convertRoleToResourceData(u *roles.Role, d *schema.ResourceData) error {
 		}
 	}
 
-	if u.UserIds != nil {
-		if err := d.Set(roleUsersKey, u.UserIds); err != nil {
-			return err
-		}
-	}
-
-	if u.GroupIds != nil {
-		if err := d.Set(roleGroupsKey, u.GroupIds); err != nil {
+	if u.PrincipalIds != nil {
+		if err := d.Set(rolePrincipalsKey, u.PrincipalIds); err != nil {
 			return err
 		}
 	}
@@ -99,16 +86,10 @@ func convertResourceDataToRole(d *schema.ResourceData) *roles.Role {
 		name := nameVal.(string)
 		u.Name = &name
 	}
-	if val, ok := d.GetOk(roleGroupsKey); ok {
-		groupIds := val.(*schema.Set).List()
-		for _, i := range groupIds {
-			u.GroupIds = append(u.GroupIds, i.(string))
-		}
-	}
-	if val, ok := d.GetOk(roleUsersKey); ok {
-		userIds := val.(*schema.Set).List()
-		for _, i := range userIds {
-			u.UserIds = append(u.UserIds, i.(string))
+	if val, ok := d.GetOk(rolePrincipalsKey); ok {
+		principalIds := val.(*schema.Set).List()
+		for _, i := range principalIds {
+			u.PrincipalIds = append(u.PrincipalIds, i.(string))
 		}
 	}
 	if val, ok := d.GetOk(roleGrantsKey); ok {
@@ -135,8 +116,7 @@ func resourceRoleCreate(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	r := convertResourceDataToRole(d)
-	users := r.UserIds
-	groups := r.GroupIds
+	principals := r.PrincipalIds
 	grants := r.Grants
 
 	newRole, apiErr, err := o.CreateRole(ctx, r)
@@ -160,13 +140,13 @@ func resourceRoleCreate(d *schema.ResourceData, meta interface{}) error {
 		}
 	}
 
-	if len(users) > 0 || len(groups) > 0 {
-		r, apiErr, err = r.SetPrincipals(ctx, groups, users)
+	if len(principals) > 0 {
+		r, apiErr, err = r.SetPrincipals(ctx, principals)
 		if apiErr != nil {
-			return fmt.Errorf("error setting principle on role: %s\n", apiErr.Message)
+			return fmt.Errorf("error setting principals on role: %s\n", apiErr.Message)
 		}
 		if err != nil {
-			return fmt.Errorf("error setting principle on role: %s\n", err)
+			return fmt.Errorf("error setting principals on role: %s\n", err)
 		}
 	}
 
@@ -218,8 +198,7 @@ func resourceRoleUpdate(d *schema.ResourceData, meta interface{}) error {
 		r.Description = &d
 	}
 
-	r.GroupIds = nil
-	r.UserIds = nil
+	r.PrincipalIds = nil
 	r.Grants = nil
 
 	r, apiErr, err := o.UpdateRole(ctx, r)
@@ -242,26 +221,18 @@ func resourceRoleUpdate(d *schema.ResourceData, meta interface{}) error {
 		}
 	}
 
-	userIDs := []string{}
-	if d.HasChange(roleUsersKey) {
-		users := d.Get(roleUsersKey).(*schema.Set).List()
-		for _, user := range users {
-			userIDs = append(userIDs, user.(string))
+	principalIds := []string{}
+	if d.HasChange(rolePrincipalsKey) {
+		principals := d.Get(rolePrincipalsKey).(*schema.Set).List()
+		for _, principal := range principals {
+			principalIds = append(principalIds, principal.(string))
 		}
 	}
 
-	groupIDs := []string{}
-	if d.HasChange(roleGroupsKey) {
-		groups := d.Get(roleGroupsKey).(*schema.Set).List()
-		for _, group := range groups {
-			groupIDs = append(groupIDs, group.(string))
-		}
-	}
-
-	if d.HasChange(roleGroupsKey) || d.HasChange(roleUsersKey) {
-		r, apiErr, err = r.SetPrincipals(ctx, groupIDs, userIDs)
+	if d.HasChange(rolePrincipalsKey) {
+		r, apiErr, err = r.SetPrincipals(ctx, principalIds)
 		if apiErr != nil || err != nil {
-			return fmt.Errorf("error updating principle on role:\n  API Err: %+v\n  Err: %+v\n", *apiErr, err)
+			return fmt.Errorf("error updating principal on role:\n  API Err: %+v\n  Err: %+v\n", *apiErr, err)
 		}
 	}
 
