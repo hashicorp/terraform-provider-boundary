ff32a7be513bcdd85f5a5a143c5be684e61c03e1
Louis Ruch <louisruch@gmail.com>
2021-01-14 13:47:34 -0800

    go generate

--- examples/resources/boundary_target/resource.tf
+++ examples/resources/boundary_target/resource.tf
@@ -12,10 +12,10 @@ resource "boundary_scope" "org" {
 }
 
 resource "boundary_scope" "project" {
-  name                     = "project_one"
-  description              = "My first scope!"
-  scope_id                 = boundary_scope.org.id
-  auto_create_admin_role   = true
+  name                   = "project_one"
+  description            = "My first scope!"
+  scope_id               = boundary_scope.org.id
+  auto_create_admin_role = true
 }
 
 resource "boundary_host_catalog" "foo" {
