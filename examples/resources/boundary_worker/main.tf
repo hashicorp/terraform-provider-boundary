f72464c8a163fd5143eece2a15962533eb645f53
Grant Orchard <go@hashicorp.com>
2022-11-10 12:45:25 +1100

    add examples for worker resource

---
+++ examples/resources/boundary_worker/main.tf
@@ -0,0 +1,13 @@
+resource "boundary_worker" "controller_led" {
+	scope_id = "global"
+	name = "worker 1"
+	description = "self managed worker with controlled led auth"
+	worker_generated_auth_token = var.worker_generated_auth_token
+}
+
+resource "boundary_self_managed_worker" "worker_led" {
+	scope_id = "global"
+	name = "worker 2"
+	description = "self managed worker with controlled led auth"
+	worker_generated_auth_token = var.worker_generated_auth_token
+}
