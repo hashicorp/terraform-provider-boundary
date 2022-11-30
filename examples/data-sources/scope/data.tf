74a1b52102836a820fcdd5ba7946cd79022d80c4
Grant Orchard <go@hashicorp.com>
2022-11-30 12:27:17 +1100

    add datasource for scopes, docs and examples

---
+++ examples/data-sources/scope/data.tf
@@ -0,0 +1,9 @@
+data "boundary_scope" "org" {
+  name                     = "SecOps"
+  scope_id                 = "global"
+}
+
+data "boundary_scope" "project" {
+	name = "2111"
+	scope_id = data.boundary_scope.id
+}
