340b6d29bf905f6250a8b1fdedd30fde323537de
Timothy Messier <tim.messier@gmail.com>
2023-02-09 10:39:09 -0500

    chore: Remove copywrites from example files (#321)
    These files are uses as input for generated doc files, as such they can
    be ignored by `copywrite`.

--- examples/resources/boundary_host_set_plugin/resource.tf
+++ examples/resources/boundary_host_set_plugin/resource.tf
@@ -1,6 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 resource "boundary_scope" "org" {
   name                     = "organization_one"
   description              = "My first scope!"
