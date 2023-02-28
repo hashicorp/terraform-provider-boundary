7799840cfb6e8c413387809ff8f9325439fa7261
Grant Orchard <go@hashicorp.com>
2023-02-28 14:44:44 +1100

    fix example attribute name

--- examples/data-sources/scope/data.tf
+++ examples/data-sources/scope/data.tf
@@ -1,9 +1,9 @@
 data "boundary_scope" "org" {
   name     = "SecOps"
-  scope_id = "global"
+  parent_scope_id = "global"
 }
 
 data "boundary_scope" "project" {
   name     = "2111"
-  scope_id = data.boundary_scope.id
+  parent_scope_id = data.boundary_scope.id
 }
