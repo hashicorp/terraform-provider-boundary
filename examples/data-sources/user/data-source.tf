9de7c9b6000bc95ee6c92b0cc0bcaa5688326e80
drewmullen <mullen.drew@gmail.com>
2023-10-11 13:40:03 -0700

    include user template for example inclusion in doc gen

---
+++ examples/data-sources/user/data-source.tf
@@ -0,0 +1,17 @@
+# Retrieve a user from the global scope
+
+data "boundary_user" "global_scope_admin" {
+  name = "admin"
+}
+
+# User from a org scope
+
+data "boundary_user" "org_user" {
+  name     = "username"
+  scope_id = data.boundary_scope.org.id
+}
+
+data "boundary_scope" "org" {
+  name            = "my-org"
+  parent_scope_id = data.boundary_scope.org.id
+}
