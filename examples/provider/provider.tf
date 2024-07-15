f6539b969c13d3c162679488f09bcc3b41f34e7e
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-07-15 10:02:58 -0400

    chore: replace deprecated arguments in provider example (#617)
    * chore: replace deprecated arguments in provider example
    
    * chore: fix extra newline
    
    * Revert "chore: fix extra newline"
    
    This reverts commit cf2de60acfcf6c9499d0306243041f950a0851f6.
    
    * Revert "chore: replace deprecated arguments in provider example"
    
    This reverts commit 49a336f7684707edb36021eef7d11bb92e4fa1b4.
    
    * chore: change example
    
    * chore: update example in README
    
    * chore: create a tests for the deprecated auth method

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,19 +1,19 @@
 provider "boundary" {
-  addr                            = "http://127.0.0.1:9200"
-  auth_method_id                  = "ampw_1234567890" # changeme
-  password_auth_method_login_name = "myuser"          # changeme
-  password_auth_method_password   = "passpass"        # changeme
+  addr                   = "http://127.0.0.1:9200"
+  auth_method_id         = "ampw_1234567890" # changeme
+  auth_method_login_name = "myuser"          # changeme
+  auth_method_password   = "passpass"        # changeme
 }
 
 provider "boundary" {
-  addr                            = "http://127.0.0.1:9200"
-  password_auth_method_login_name = "myuser"
-  password_auth_method_password   = "passpass"
+  addr                   = "http://127.0.0.1:9200"
+  auth_method_login_name = "myuser"
+  auth_method_password   = "passpass"
 }
 
 provider "boundary" {
-  addr                            = "http://127.0.0.1:9200"
-  password_auth_method_login_name = "myuser"
-  password_auth_method_password   = "passpass"
-  scope_id                        = "s_1234567890"
+  addr                   = "http://127.0.0.1:9200"
+  auth_method_login_name = "myuser"
+  auth_method_password   = "passpass"
+  scope_id               = "s_1234567890"
 }
