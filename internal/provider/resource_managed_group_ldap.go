3ccbe568670690f7afe98609e182ac6363e5d82d
abhi99shek99 <abhishek.tanwar9@ibm.com>
2026-09-15 14:13:54 +0530

    Moved DiffSuppressFunc onto the string Elem schema so it's applied per-element instead of the list itself

--- internal/provider/resource_managed_group_ldap.go
+++ internal/provider/resource_managed_group_ldap.go
@@ -53,12 +53,12 @@ func resourceManagedGroupLdap() *schema.Resource {
 			managedGroupLdapGroupNamesKey: {
 				Description: "The list of groups that make up the managed group.",
 				Type:        schema.TypeList,
+				Required:    true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
-				},
-				Required: true,
-				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
-					return strings.EqualFold(old, new)
+					DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+						return strings.EqualFold(old, new)
+					},
 				},
 			},
 		},
